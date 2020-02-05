cd439132590ea9a273f7e91dfb636fa9d0963a26
Csaba <56678429+CsabaVK@users.noreply.github.com>
2020-02-05 09:03:45 +0100

    Create INSERT.sql

---
+++ SQL táblák/INSERT.sql
@@ -0,0 +1,8 @@
+INSERT INTO `s_cikkek` (nev, near, afa, keszlet, mennymegy) VALUES
+('alma', '569', '27', '152.65', 'kg'),
+('tej', '179', '27', '521', 'doboz'),
+('tévé', '162871', '27', '4', 'darab'),
+('sertés comb', '1625.23', '5', '36', 'kg'),
+('narancslé', '221.5', '27', '114', 'doboz'),
+('körte', '568', '27', '34', 'kg')
+;
